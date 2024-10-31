d9ad5fcda680d0a3d181749a9fa4a3113dfae129
Your Name <you@example.com>
2024-10-31 08:05:16 +0000

    sync changes

--- lab01/main.tf
+++ lab01/main.tf
@@ -6,45 +6,45 @@ variable "location" {
   default = "East US"
 }
 
-resource "azurerm_resource_group" "rg-beno" {
+resource "azurerm_resource_group" "rg" {
   name     = "beno-resources"
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet-beno" {
-  name                = "beno-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg-beno.name
-}
 
-resource "azurerm_subnet" "subnet-beno" {
+resource "azurerm_subnet" "subnet" {
   name                 = "beno-subnet"
-  resource_group_name  = azurerm_resource_group.rg-beno.name
-  virtual_network_name = azurerm_virtual_network.vnet-beno.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 
-
-resource "azurerm_public_ip" "pip-beno" {
-  name                = "beno-pip"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "beno-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg-beno.name
-  allocation_method   = "Dynamic"  
-  sku = "Basic"  
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_network_interface" "nic-beno" {
+resource "azurerm_public_ip" "pip" {
+  name                = "beno-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"  # Dynamic IP allocation for Basic SKU
+  sku = "Basic"  # Use Basic SKU (Stock Keeping Unit - azure tiers) for dynamic IP
+}
+
+resource "azurerm_network_interface" "nic" {
   name                = "beno-nic"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg-beno.name
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "beno-ipconfig"
-    subnet_id                     = azurerm_subnet.subnet-beno.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip-beno.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
@@ -54,7 +54,7 @@ variable "vm_size" {
 }
 
 variable "admin_username" {
-  default = "adminuser-beno"
+  default = "adminuser"
 }
 
 variable "admin_password" {
@@ -62,11 +62,11 @@ variable "admin_password" {
 }
 
 
-resource "azurerm_linux_virtual_machine" "vm-beno" {
+resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "beno-vm"
   location              = var.location
-  resource_group_name   = azurerm_resource_group.rg-beno.name
-  network_interface_ids = [azurerm_network_interface.nic-beno.id]
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
   size                  = var.vm_size
 
   os_disk {
@@ -90,12 +90,8 @@ resource "azurerm_linux_virtual_machine" "vm-beno" {
   computer_name = "beno-vm"
 }
 
-resource "time_sleep" "wait_for_ip" {
-  create_duration = "30s"  # Wait for 30 seconds
-}
 
 output "vm_public_ip" {
-  value = azurerm_public_ip.pip-beno.ip_address
+  value = azurerm_public_ip.pip.ip_address
   description = "Public IP address of the VM"
 }
-
